175f6c37bcd6e8132350912727407afb33d24f5f
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2024-05-23 11:51:36 -0400

    Add Entities

---
+++ Entities/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace UseCase.Entities;
+
+public class Contact
+{
+    public Guid Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string? Phone { get; set; }
+    public Guid Owner { get; set; }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace UseCase.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string? Firstname { get; set; }
+    public string? Lastname { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+}
